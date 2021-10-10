fc69ca9ff1a008a376754394c8bdc8f239ec8435
b0ho <ybg4828@gmail.com>
2021-10-10 17:15:58 +0900

    람다 함수 등록
    로그 테스트

--- lambda/getCards.js
+++ lambda/getCards.js
@@ -1,9 +1,9 @@
 exports.handler = async (event) => {
-    console.log("Received: "+JSON.stringify(event,null,2));
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  console.log("receive: " + JSON.stringify(event, null, 2));
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
 };
